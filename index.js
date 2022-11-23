427a65aacb8a7474ab651e8c49ac2a3fa85de91f
fyxyuc ucycy <shramov058@gmail.com>
2022-11-23 19:56:11 +0300

    converter

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-let userName = 'abbas';
-console.log("hello " + userName); 
-userName= 'Sergei ';
-alert(userName +  'любит джава');
+let a = prompt('Введите количество $?');
+const euro = 70
+const dollar = 60;
+
+let e = a * dollar
+alert(e);
+let b = prompt('Введите количество euro?');
+
+let j = b * euro
+alert(j);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
