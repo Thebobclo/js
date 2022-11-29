7f8e4c7275f0e59e2ea5de3121fd126ecbc5dd96
fyxyuc ucycy <shramov058@gmail.com>
2022-11-29 19:48:37 +0300

    ls4zag

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-let lengthA = prompt('Введите длину основания трапеции a');
-let lengthB = prompt('Введите длину основания трапеции b');
-let height= prompt('Введите высоту основания трапеции h');
-let resultSquare = ((lengthA+lengthB)/2*height);
-alert("Площадь трапеции " +resultSquare) ;
+let answerPpl = String(prompt("Не огонь , а жжётся? "));
+let summ = null;
+let summError = null;
+if (answerPpl == "крапива" || answerPpl == "Крапива") {
+  alert("Вы угадали");
+  summ++;
+  alert("Игра окончена");
+} else {
+  alert("Вы не угадали ");
+  summError++;
+}
 
+let answerPpltwo = String(prompt("Кругом вода,а с питьем беда? "));
+if (answerPpltwo == "море" || answerPpltwo == "Море") {
+  alert("Вы угадали");
+  summ++;
+  alert("Игра окончена");
+} else {
+  alert("Вы не угадали");
+  summError++;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let answerPplthree = String(prompt("К вечеру умирает,поутру оживает? "));
+if (answerPplthree == "День" || answerPplthree == "день") {
+  alert("Вы угадали");
+  summ++;
+  alert("Игра окончена");
+} else {
+  alert("Вы не угадали");
+  summError++;
+}
+alert("Сделано задач верно: " + summ + "Сделано задач не верно: " + summError);
